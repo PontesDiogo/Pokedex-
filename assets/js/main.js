f1167c2e7fd1f7d2470b1a68410afa6e8385d40d
deogo <diogogpontes@gmail.com>
2024-11-29 09:47:24 -0300

    Corrigindo erro no javascript e trazendo as informações dos pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,14 +4,14 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=&{offset}&limit=${limit}`
 
 function ConvertPokemonToHtml(pokemon) {
     return `
-    <li class="pokemon ${pokemon.type}">
-                <span class="number">${pokemon.number}</span>
-                <span class="name">${pokemon.name}</span>
+    <li class="pokemon ">
+                <span class="number">${pokemon?.id}</span>
+                <span class="name">${pokemon?.name}</span>
                 <div class="detail">
-                    <ol class="types">
-                        ${pokemon.types.map((type) => `<li class="type">Grass</li>`).join('')}
+                 <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type">${type.type.name}</li>`).join('')}
                     </ol>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/1.png"
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/${pokemon.id}.png"
                         alt="${pokemon.name}">
 
                 </div>
@@ -19,20 +19,27 @@ function ConvertPokemonToHtml(pokemon) {
     `
 }
 
+const pokemonList = document.getElementById('pokemonList')
+let isLoading = true
+
+fetch(url).then(async (response) => {
+    const pokemonsReponse = await response.json();
+    const pokemons = pokemonsReponse.results
+
+    console.log(pokemons[1])
+
+    for (let i = 0; i < 10; i++) {
+        const object = pokemons[i]
+        const pokemonDetails = await fetch(object.url)
+        const pokemon = await pokemonDetails.json()
+        console.log(pokemon)
+        pokemonList.innerHTML += ConvertPokemonToHtml(pokemon)
+    }
+}).catch((error) => console.log(error))
+.finally(() => isLoading = false)
+
+
 fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
-    .then((pokemonList) =>{
-        for(let i =0; i<pokemonList.length; i++){
-            const pokemon = pokemonList[i]
-            console.log(ConvertPokemonToHtml(pokemon))
-
-            document.getElementByIdClassName('pokemons').innerHTML += ConvertPokemonToHtml(pokemon)
-        }
-    })
-
-    .catch((error) => console.log(error))
-
 
 
 
